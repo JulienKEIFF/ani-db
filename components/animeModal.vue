0c2426f1f08f875390cba7bc5a10820b4dd239ac
Julien <j.keiff@hotmail.fr>
2021-06-04 11:03:06 +0200

    add base anime modal

---
+++ components/animeModal.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="add-entry-form" class="absolute top-0 left-32 w-10/12 bg-red-500 flex flex-col p-8 mt-8 mb-4 z-50">
+      <label for="title">Titre</label>
+      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="title" type="text" v-model="content.title" />
+
+      <label for="descr">Description</label>
+      <textarea class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="descr" type="text" v-model="content.description" />
+
+      <label for="season">Saison</label>
+      <select class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none bg-white" id="season" v-model="content.season">
+        <option value="printemps">Printemps</option>
+        <option value="ete">Eté</option>
+        <option value="automne">Automne</option>
+        <option value="hiver">Hiver</option>
+      </select>
+
+      <label for="status">Status</label>
+      <select class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none bg-white" id="status" v-model="content.status">
+        <option value="termine">Terminé</option>
+        <option value="encours">En cours</option>
+        <option value="pasencoresorti">Pas encore sorti</option>
+        <option value="annule">Annulé</option>
+      </select>
+
+      <label for="episode">Nombre d'épisodes</label>
+      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="episode" type="number" v-model="content.episodes" />
+
+      <label for="episode-duration">Durée d'un épisode</label>
+      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="episode-duration" type="number" v-model="content.episodeDuration" />
+
+      <label for="score">Score</label>
+      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="score" type="number" v-model="content.score" />
+
+      <label for="studio">Studio d'animation</label>
+      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="studio" type="text" v-model="content.studios" />
+
+      <label for="genres">Genres (séparée par une virgule)</label>
+      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="genres" type="text" v-model="content.genres" />
+
+      <label for="source">Source</label>
+      <select class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none bg-white" id="source" v-model="content.source">
+        <option value="manga">Manga</option>
+        <option value="original">Original</option>
+        <option value="lightnovel">Light Novel</option>
+        <option value="visualnovel">Visual Novel</option>
+        <option value="novel">Novel</option>
+        <option value="anime">Anime</option>
+        <option value="videogame">Jeu Vidéo</option>
+        <option value="doujinshi">Doujinshi</option>
+        <option value="other">Autre</option>
+      </select>
+
+      <label for="img">Couverture</label>
+      <input class="mb-5 p-3 focus:border-blue-400 rounded outline-none" type="file" accept="image/*" id="img"  />
+
+      <button v-if="!update" id="add-entry-btn" class="mx-auto bg-blue-400 hover:bg-blue-500 text-white font-bold p-2 rounded w-80" @click="addEntry()">Ajouter un anime</button>
+      <button v-if="update" id="add-entry-btn" class="mx-auto bg-blue-400 hover:bg-blue-500 text-white font-bold p-2 rounded w-80" @click="applyUpdate()">Modifier l'anime</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "anime-modal",
+  props: ['content'],
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
--- components/manage.vue
+++ components/manage.vue
@@ -1,63 +1,5 @@
 <template>
   <div class="m-auto mt-4 w-11/12 bg-blue-100 rounded-md p-4 mb-8">
-    <div id="add-entry-form" class="mt flex flex-col border-b-2 border-blue-300 pb-8 mb-4">
-      <label for="title">Titre</label>
-      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="title" type="text" v-model="newAnime.title" />
-
-      <label for="descr">Description</label>
-      <textarea class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="descr" type="text" v-model="newAnime.description" />
-
-      <label for="season">Saison</label>
-      <select class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none bg-white" id="season" v-model="newAnime.season">
-        <option value="printemps">Printemps</option>
-        <option value="ete">Eté</option>
-        <option value="automne">Automne</option>
-        <option value="hiver">Hiver</option>
-      </select>
-
-      <label for="status">Status</label>
-      <select class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none bg-white" id="status" v-model="newAnime.status">
-        <option value="termine">Terminé</option>
-        <option value="encours">En cours</option>
-        <option value="pasencoresorti">Pas encore sorti</option>
-        <option value="annule">Annulé</option>
-      </select>
-
-      <label for="episode">Nombre d'épisodes</label>
-      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="episode" type="number" v-model="newAnime.episodes" />
-
-      <label for="episode-duration">Durée d'un épisode</label>
-      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="episode-duration" type="number" v-model="newAnime.episodeDuration" />
-
-      <label for="score">Score</label>
-      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="score" type="number" v-model="newAnime.score" />
-
-      <label for="studio">Studio d'animation</label>
-      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="studio" type="text" v-model="newAnime.studios" />
-
-      <label for="genres">Genres (séparée par une virgule)</label>
-      <input class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none" id="genres" type="text" v-model="newAnime.genres" />
-
-      <label for="source">Source</label>
-      <select class="mb-5 p-3 focus:border-blue-400 rounded border-2 outline-none bg-white" id="source" v-model="newAnime.source">
-        <option value="manga">Manga</option>
-        <option value="original">Original</option>
-        <option value="lightnovel">Light Novel</option>
-        <option value="visualnovel">Visual Novel</option>
-        <option value="novel">Novel</option>
-        <option value="anime">Anime</option>
-        <option value="videogame">Jeu Vidéo</option>
-        <option value="doujinshi">Doujinshi</option>
-        <option value="other">Autre</option>
-      </select>
-
-      <label for="img">Couverture</label>
-      <input class="mb-5 p-3 focus:border-blue-400 rounded outline-none" type="file" accept="image/*" id="img"  />
-
-      <button v-if="!update" id="add-entry-btn" class="mx-auto bg-blue-400 hover:bg-blue-500 text-white font-bold p-2 rounded w-80" @click="addEntry()">Ajouter un anime</button>
-      <button v-if="update" id="add-entry-btn" class="mx-auto bg-blue-400 hover:bg-blue-500 text-white font-bold p-2 rounded w-80" @click="applyUpdate()">Modifier l'anime</button>
-    </div>
-
 
     <div v-for="anime in animeList" :key="anime.id" class="relative flex flex-row items-center odd:bg-blue-500">
       <img :src="anime.cover[0] ? apiBase + anime.cover[0].url : ''" class="w-16 h-16 mr-4 my-2 rounded-full" />
@@ -67,6 +9,7 @@
     </div>
 
     <modal :name="'utilisateur'" v-if="modal" @validate="removeEntry" @cancel="modal = false" />
+    <anime-modal v-if="animeModal" :content="newAnime" />
 
   </div>
 </template>
@@ -92,14 +35,18 @@ export default {
       },
       idUpdate: null,
       removeId: null,
-      update: false,
       modal: false,
+      animeModal: false,
     }
   },
   methods: {
     removeModal: function(id) {
       this.removeId = id;
       this.modal = true;
+    },
+    updateEntries: function() {
+      this.$axios.get('anime-shows')
+      .then(res => { this.animeList = res.data })
     },
     removeEntry: function() {
       this.modal = false
@@ -110,12 +57,7 @@ export default {
       this.$axios.post('anime-shows', this.newAnime)
       this.updateEntries()
     },
-    updateEntries: function() {
-      this.$axios.get('anime-shows')
-        .then(res => { this.animeList = res.data })
-    },
     editEntry: function(anime) {
-      this.update = true;
       this.idUpdate = anime.id;
       this.newAnime.title = anime.title;
       this.newAnime.description = anime.description;
@@ -127,6 +69,8 @@ export default {
       this.newAnime.studios = anime.studios;
       this.newAnime.genres = anime.genres;
       this.newAnime.source = anime.source;
+      this.animeModal = true;
+
     },
     applyUpdate: function() {
       this.update = false;
